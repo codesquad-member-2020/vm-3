cbba6339bd9ed10e6ff7d14c5f94879c4fe52290
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-03-11 18:42:34 +0900

    [수정] 변수 추가 및 최초 구동에 대한 코드 추가
    변수 추가 및 최초 구동에 대한 코드 추가 >
    * client 가 init data 요청을 할 때 상태를 초기화.
    * client 의 init data 요청에 대한 데이터를 response.

--- Server/Server.js
+++ Server/Server.js
@@ -14,6 +14,34 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+const productListJSON = fs.readFileSync('productData.json', 'utf8');
+const productList = JSON.parse(productListJSON);
+let collectedCashArray = [0, 0, 0, 0, 0, 0, 0];
+let walletCashArray = [0, 1, 5, 5, 2, 2, 1];
+let collectedCash = 0;
+let walletCash = 0;
+
 https.createServer(options, app).listen(port, function(){
     console.log(`App listening on port ${port}!`)
 });
+
+app.get('/get/initial-data', (req, res) => {
+    initializeValue();
+    const initializeData = {};
+
+    initializeData.collectedCash = collectedCash;
+    initializeData.walletCash = walletCash;
+    initializeData.walletCashArray = walletCashArray;
+    initializeData.productList = productList;
+    initializeData.message = ["자판기 구동이 시작되었습니다."];
+    
+    res.send(initializeData)
+});
+
+function initializeValue() {
+    collectedCashArray = [0, 0, 0, 0, 0, 0, 0];
+    walletCashArray = [0, 1, 5, 5, 2, 2, 1];
+    collectedCash = 0;
+    walletCash = 0;
+    inputNumber = 0;
+}
