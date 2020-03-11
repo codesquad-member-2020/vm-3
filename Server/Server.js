42a701df5ad72948c443f6e6c5b9b97221a01047
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-03-11 21:21:19 +0900

    [수정] 사용자의 상품 버튼 클릭에 대해 서버에 요청
    사용자의 상품 버튼 클릭에 대해 서버에 요청 >
    * 상품 가격과 투입 가격 비교 후 response 메시지 작성
    * 상품이 판매되어 collectedCash 가 변경 될 경우 response 에 포함

--- Server/Server.js
+++ Server/Server.js
@@ -14,16 +14,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+const priceTypeCount = 7;
 const productListJSON = fs.readFileSync('productData.json', 'utf8');
 const productList = JSON.parse(productListJSON);
 let collectedCashArray = [0, 0, 0, 0, 0, 0, 0];
 let walletCashArray = [0, 1, 5, 5, 2, 2, 1];
 let collectedCash = 0;
 let walletCash = 0;
-
-https.createServer(options, app).listen(port, function(){
-    console.log(`App listening on port ${port}!`)
-});
 
 app.get('/get/initial-data', (req, res) => {
     initializeValue();
@@ -38,6 +35,27 @@ app.get('/get/initial-data', (req, res) => {
     res.send(initializeData)
 });
 
+app.post('/patch/product-button-click', (req, res) => {
+    const selectedItemIndex = req.body.index;
+    const selectedItemPrice = productList[selectedItemIndex].price;
+    const selectedItemName = productList[selectedItemIndex].name;
+    const productButtonClickResponse = {};
+
+    if (selectedItemPrice > collectedCash) {
+        productButtonClickResponse.message = ["투입된 금액이 부족합니다."]
+    }
+    else {
+        productButtonClickResponse.message = [`${selectedItemName} 이 판매되었습니다`];
+
+        collectedCash = collectedCash - selectedItemPrice;
+        collectedCashArray = makeCollectedCashArray(collectedCash);
+
+        productButtonClickResponse.collectedCash = collectedCash;
+    }
+
+    res.send(productButtonClickResponse);
+});
+
 function initializeValue() {
     collectedCashArray = [0, 0, 0, 0, 0, 0, 0];
     walletCashArray = [0, 1, 5, 5, 2, 2, 1];
@@ -45,3 +63,25 @@ function initializeValue() {
     walletCash = 0;
     inputNumber = 0;
 }
+
+function makeCollectedCashArray(collectedCash) {
+    const collectedCashArray = [];
+
+    for (let index = 0 ; index < priceTypeCount ; ++index) {
+        collectedCashArray.push(0);
+    }
+
+    collectedCashArray[6] = collectedCash / 10000;
+    collectedCashArray[5] = (collectedCash % 10000) / 5000;
+    collectedCashArray[4] = ((collectedCash % 10000) % 5000) / 1000;
+    collectedCashArray[3] = (((collectedCash % 10000) % 5000) % 1000) / 500;
+    collectedCashArray[2] = ((((collectedCash % 10000) % 5000) % 1000 ) % 500) / 100;
+    collectedCashArray[1] = (((((collectedCash % 10000) % 5000) % 1000 ) % 500) % 100) / 50;
+    collectedCashArray[0] = ((((((collectedCash % 10000) % 5000) % 1000 ) % 500) % 100) % 50) / 10;
+
+    return collectedCashArray;
+}
+
+https.createServer(options, app).listen(port, function(){
+    console.log(`App listening on port ${port}!`)
+});
